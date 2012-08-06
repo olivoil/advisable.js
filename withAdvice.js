6a48d879b7dd06111d14c1096fffb4b664ce061a
Olivier Melcher <olivier.melcher@gmail.com>
2012-08-06 09:13:53 -0400

    new release

--- withAdvice.js
+++ withAdvice.js
@@ -3,16 +3,14 @@
  * Copyright(c) 2012 Olivier Melcher <olivier.melcher@gmail.com>
  * MIT Licensed
  */
-
-// With Advice
-// -----------
-//
-// An Aspect-Oriented Library for javascript.
-// Inspired by [@angustweets and @danwrong](https://speakerdeck.com/u/anguscroll/p/how-we-learned-to-stop-worrying-and-love-javascript).
-//
-// <script async class="speakerdeck-embed" data-id="4fc7e727ed0e1d001f022749" data-ratio="1.3333333333333333" src="//speakerdeck.com/assets/embed.js"></script>
-
 ;(function(){
+
+
+  // With Advice
+  // -----------
+  //
+  // An Aspect-Oriented Library for javascript.
+  // Inspired by [@angustweets and @danwrong](https://speakerdeck.com/u/anguscroll/p/how-we-learned-to-stop-worrying-and-love-javascript).
 
   // Library version.
 
